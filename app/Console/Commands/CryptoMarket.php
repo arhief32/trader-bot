7375e4548fb78d10c9020bc50bcac6a5f9bef83a
Arief Muarrief <arief.muarriefbuhari@work.bri.co.id>
2022-09-27 20:39:28 +0700

    fix

--- app/Console/Commands/CryptoMarket.php
+++ app/Console/Commands/CryptoMarket.php
@@ -33,56 +33,42 @@ class CryptoMarket extends Command
         // $assets = env('ASSETS');
         // $assets = explode(',', $assets);
         // $assets = ['bitcoin','ethereum','binance-coin','xrp','cardano','solana','terra-luna','dogecoin','polkadot','polygon','shiba-inu','tron','avalanche','ethereum-classic','litecoin','cosmos','chainlink','monero','algorand','chiliz'];
-        $assets = ['BTCUSDT','ETHUSDT','BNBUSDT','XRPUSDT','ADAUSDT','SOLUSDT','LUNAUSDT','DOGEUSDT','DOTUSDT','MATICUSDT','SHIBUSDT','TRXUSDT','AVAXUSDT','ETCUSDT','LTCUSDT','ATOMUSDT','LINKUSDT','XMRUSDT','ALGOUSDT','CHZUSDT'];
+        $assets = ['BTCUSDT','ETHUSDT','BNBUSDT','XRPUSDT','ADAUSDT','SOLUSDT','LUNAUSDT','DOGEUSDT','DOTUSDT','MATICUSDT','TRXUSDT','AVAXUSDT','ETCUSDT','LTCUSDT','ATOMUSDT','LINKUSDT','XMRUSDT','ALGOUSDT','CHZUSDT'];
         
         foreach($assets as $asset){
             // request to get price real-time
-            $client_average_price = new \GuzzleHttp\Client([
-                'base_uri' => 'https://api.binance.com/api',
+            $client_klines = new \GuzzleHttp\Client([
+                'base_uri' => 'https://fapi.binance.com/api',
                 'verify'=> false,
                 'debug' => false, // optional
             ]);
-            $request_average_price = $client_average_price->request('GET', '/api/v3/avgPrice?symbol='.$asset);
-            $response_average_price = json_decode($request_average_price->getBody());
+            $request_klines = $client_klines->request('GET', '/fapi/v1/klines', [
+                'query' => [
+                    'symbol' => $asset,
+                    'interval' => '1m',
+                    'limit' => '60',
+                ],
+            ]);
+            $response_klines = json_decode($request_klines->getBody());
             
-            Log::channel('cryptomarket')->info(json_encode($response_average_price));
-
-            // get market redis
-            $get_market = Redis::get('market_'.$asset);
-            $get_market = json_decode($get_market);
-            $get_market == null ? $asset_prices = [] : $asset_prices = $get_market;
-                    
-            // // insert to row market
-            $response_average_price->price > 1 ? $response_average_price->price = round($response_average_price->price, 4) : false;
-            $response_average_price->price < 1 && $response_average_price->price > 0.1 ? $response_average_price->price = round($response_average_price->price, 4) : false;
-            $response_average_price->price < 0.1 && $response_average_price->price > 0.01 ? $response_average_price->price = round($response_average_price->price, 5) : false;
-            $response_average_price->price < 0.01 && $response_average_price->price > 0.001 ? $response_average_price->price = round($response_average_price->price, 6) : false;
-            $response_average_price->price < 0.001 && $response_average_price->price > 0.0001 ? $response_average_price->price = round($response_average_price->price, 7) : false;
-            $asset_price = [
-                'price_usd' => $response_average_price->price,
-            ];
-            array_push($asset_prices, $asset_price);
-
-            // // remove if > 100
-            if(count($asset_prices)  > 7){
-                array_shift($asset_prices);
-            }
+            Log::channel('cryptomarket')->info(json_encode($response_klines));
 
             // // calculate indicator
-            $array_indicator = [];
-            if($asset_prices != null || $asset_prices != 0){
-                foreach($asset_prices as $row_asset){
-                    if(isset($row_asset->price_usd)){
-                        array_push($array_indicator, $row_asset->price_usd);
-                    }
+            $data_market = [];
+            if($response_klines != null || $response_klines != 0){
+                foreach($response_klines as $price){
+                    array_push($data_market, $price[4]);
                 }
             }
-            $rsi = trader_rsi($array_indicator, 6);
+            // rsi
+            $rsi = trader_rsi($data_market, 6);
             isset($rsi[6]) ? $rsi = $rsi[6] : $rsi = false;
+            // macd
+            $macd = trader_macd($data_market, 12, 26,9);
             
             // update market in redis (del and then set)
             Redis::del('market_'.$asset);
-            Redis::set('market_'.$asset, json_encode($asset_prices));
+            Redis::set('market_'.$asset, json_encode($data_market));
 
             Log::build([
                 'driver' => 'daily',
@@ -90,9 +76,9 @@ class CryptoMarket extends Command
                 'level' => env('LOG_LEVEL', 'info'),
                 'days' => 14,
             ])->info(json_encode([
-                'price_usd' => $asset_price['price_usd'],
+                'price_usd' => end($data_market),
                 'rsi' => $rsi,
-                'array_indicator' => $array_indicator,
+                'macd' => $macd[0]['57'],
             ]));
         }
     }
